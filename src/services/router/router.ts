4fdbd65f440e48428fb1efa303f5e93713b1bd53
Desclim <declownmail@gmail.com>
2026-03-13 03:40:33 +0300

    netlify log

--- src/services/router/router.ts
+++ src/services/router/router.ts
@@ -1,33 +1,21 @@
-import {render} from "../../utils/render";
-import {LoginPage} from "../../pages/login/login";
-import type {Route} from "./types";
-import {initLoginPage} from "../../pages/login/init";
+import { render } from "../../utils/render";
+import type { Route } from "./types";
 
-const routes:Record<string, Route> = {
+const routes: Record<string, Route> = {
     '/': {
-        render: LoginPage,
-        init: initLoginPage
+        render: () => '<h1>router works</h1>',
     },
 };
 
 export function router() {
     const path = window.location.pathname;
-    let route:Route = routes[path];
-
-    if (!route) {
-        window.history.replaceState({}, '', '/404');
-        route = routes['/404'];
-    }
-
-    if (!route) {
-        return
-    }
+    const route: Route = routes[path] ?? routes['/'];
 
     render(route.render());
     route.init?.();
 }
 
-export function navigate(path:string) {
+export function navigate(path: string) {
     window.history.pushState({}, '', path);
     router();
 }
