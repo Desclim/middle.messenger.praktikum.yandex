26220aadab5a46f5bf0d43484f46ec5f22a1c076
Desclim <declownmail@gmail.com>
2026-03-13 03:24:26 +0300

    netlify log

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import './styles/global.scss';
 
-document.body.innerHTML = '<h1 style="font-size: 48px; color: red;">TEST</h1>';
+const app = document.querySelector('#app');
+
+if (!app) {
+    throw new Error('app not found');
+}
+
+app.innerHTML = '<h1 style="font-size: 48px; color: red;">TEST</h1>';
