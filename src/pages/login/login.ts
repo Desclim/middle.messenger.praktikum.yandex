efeab84c7a7c11accf19436803ad9fcf880699bb
Desclim <declownmail@gmail.com>
2026-03-13 04:30:04 +0300

    netlify log

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,4 +1,4 @@
-import Handlebars from "handlebars/dist/handlebars.js";
+import Handlebars from "handlebars";
 import template from "./login.hbs?raw";
 import "./login.scss";
 
--- src/types/handlebars.d.ts
+++ src/types/handlebars.d.ts
@@ -2,8 +2,3 @@ declare module '*.hbs?raw' {
     const template: string;
     export default template
 }
-
-declare module "handlebars/dist/handlebars.js" {
-    import Handlebars from "handlebars";
-    export default Handlebars;
-}
