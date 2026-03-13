51667e2c4616fe5f3cf10f39ccd32af94af08f15
Desclim <declownmail@gmail.com>
2026-03-13 04:31:24 +0300

    netlify log

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,19 +1,9 @@
-import Handlebars from "handlebars";
 import template from "./login.hbs?raw";
-import "./login.scss";
 
 export function LoginPage(): string {
-    try {
-        const compiled = Handlebars.compile(template);
-        return compiled({});
-    } catch (error) {
-        const message = error instanceof Error ? error.message : String(error);
-
-        return `
-            <pre style="padding:16px;white-space:pre-wrap;color:red;">
-LOGIN TEMPLATE ERROR:
-${message}
-            </pre>
-        `;
-    }
+    return `
+        <pre style="padding:16px;white-space:pre-wrap;">
+${template.replace(/</g, "&lt;")}
+        </pre>
+    `;
 }
