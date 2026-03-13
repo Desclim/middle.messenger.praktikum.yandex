4afd7acc94172b9be23523135ec76c4dde0b92a4
Desclim <declownmail@gmail.com>
2026-03-13 03:35:00 +0300

    netlify log

--- src/services/router/router.ts
+++ src/services/router/router.ts
@@ -1,71 +1,29 @@
-import {render} from "../../utils/render";
-import {LoginPage} from "../../pages/login/login";
-import type {Route} from "./types";
-import {initLoginPage} from "../../pages/login/init";
-import {NotFoundPage} from "../../pages/not-found/not-found";
-import {ErrorPage} from "../../pages/error/error";
-import {RegisterPage} from "../../pages/register/register";
-import {initRegisterPage} from "../../pages/register/init"
-import {ChatsPage} from "../../pages/chats/chats";
-import {initChatsPage} from "../../pages/chats/init";
-import {ProfilePage} from "../../pages/profile/profile";
-import {initProfilePage} from "../../pages/profile/init";
-import {EditProfilePage} from "../../pages/edit-profile/edit-profile";
-import {initEditProfilePage} from "../../pages/edit-profile/init";
-import {EditPasswordPage} from "../../pages/edit-password/edit-password";
-import {initEditPasswordPage} from "../../pages/edit-password/init";
+import { render } from "../../utils/render";
+import type { Route } from "./types";
 
-const routes:Record<string, Route> = {
+const routes: Record<string, Route> = {
     '/': {
-        render: LoginPage,
-        init: initLoginPage
-    },
-    '/404': {
-        render: NotFoundPage
-    },
-    '/500': {
-        render: ErrorPage
-    },
-    '/register': {
-        render:RegisterPage,
-        init:initRegisterPage
-    },
-    '/chats': {
-        render:ChatsPage,
-        init:initChatsPage
-    },
-    '/profile': {
-        render:ProfilePage,
-        init: initProfilePage,
-    },
-    '/edit-profile': {
-        render: EditProfilePage,
-        init: initEditProfilePage,
-    },
-    '/edit-password': {
-        render: EditPasswordPage,
-        init:initEditPasswordPage,
+        render: () => '<h1>router works</h1>',
     },
 };
 
 export function router() {
     const path = window.location.pathname;
-    let route:Route = routes[path];
+    let route: Route = routes[path];
 
     if (!route) {
-        window.history.replaceState({}, '', '/404');
-        route = routes['/404'];
+        route = routes['/'];
     }
 
     if (!route) {
-        return
+        return;
     }
 
     render(route.render());
     route.init?.();
 }
 
-export function navigate(path:string) {
+export function navigate(path: string) {
     window.history.pushState({}, '', path);
     router();
 }
